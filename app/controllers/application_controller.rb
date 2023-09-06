f0784c69b073707e9c6b8cc1075d3ff15b70ccd1
Aryokonon <arnon.yokoyama@gmail.com>
2023-09-06 20:19:08 +0900

    render deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:index, :show]  # Allow unauthenticated access to index and show actions.
 
   private
 
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,17 +1,17 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :check_owner, only: [:edit, :update]
+  before_action :check_ownership, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.all
   end
-  
+
   def new
     @prototype = Prototype.new
   end
 
-  def create  
+  def create
     @prototype = current_user.prototypes.build(prototype_params)
     if @prototype.save
       redirect_to root_path, notice: 'Prototype was successfully created.'
@@ -20,7 +20,7 @@ class PrototypesController < ApplicationController
     end
   end
 
-  def edit    
+  def edit
     if current_user != @prototype.user
       redirect_to root_path, alert: "You are not authorized to edit this prototype."
     end
@@ -44,18 +44,17 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments
-  
-    if user_signed_in? && current_user == @prototype.user
+
+    if user_signed_in?
       @user = @prototype.user
-      @user_owns_prototype = true
+      @user_owns_prototype = current_user == @prototype.user
     else
       @user_owns_prototype = false
     end
   end
-    
+
   private
 
   def prototype_params
@@ -66,9 +65,8 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
-  def check_owner
-    unless user_signed_in? && current_user == @prototype.user
-      redirect_to root_path, alert: "You are not authorized to edit this prototype."
-    end
+  def check_ownership
+    @prototype = Prototype.find(params[:id])
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
